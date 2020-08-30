fb9d9318bf4508b081c50a4b8d6fc79d09a8d390
pratik <quantic.pratik.shetty@investree.id>
2020-08-30 13:01:25 +0530

    added cors

--- src/main/java/ind/automation/salesexpense/controller/BankStatementController.java
+++ src/main/java/ind/automation/salesexpense/controller/BankStatementController.java
@@ -6,6 +6,7 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -24,7 +25,8 @@ public class BankStatementController {
 
 	@Autowired
 	private BankStatementService service;
-
+	
+	@CrossOrigin
 	@PostMapping(value = "/processbankstatement")
 	public ResponseEntity<BankStatementResponseBean> uploadDocuments(@RequestPart("file") MultipartFile file,
 			@RequestPart("vendorlistdata") String vendorlist)
--- src/main/java/ind/automation/salesexpense/controller/GenerateVendorListController.java
+++ src/main/java/ind/automation/salesexpense/controller/GenerateVendorListController.java
@@ -6,6 +6,7 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -25,7 +26,8 @@ public class GenerateVendorListController {
 
 	@Autowired
 	private BankStatementConversionService bankStatementConversionService;
-
+	
+	@CrossOrigin
 	@PostMapping(value = "/uploaddocuments")
 	public ResponseEntity<FullVendorListResponseBean> uploadDocuments(
 			@RequestPart("balanceSheetDetails") MultipartFile balanceSheetDetails,
@@ -34,7 +36,8 @@ public class GenerateVendorListController {
 		FullVendorListResponseBean bean = balanceSheetService.modifyBalanceSheet(balanceSheetDetails, acccountList);
 		return new ResponseEntity<>(bean, HttpStatus.OK);
 	}
-
+	
+	@CrossOrigin
 	@PostMapping(value = "/uploadbankstatement")
 	public ResponseEntity<BankStatementResponseListResponse> uploadBankstatement(
 			@RequestPart("file") MultipartFile file) throws Exception {
