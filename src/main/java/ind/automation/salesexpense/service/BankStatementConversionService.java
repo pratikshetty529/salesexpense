5bf4eda97beb1c220fd261d0bc64fe4c4da33ae1
pratik <quantic.pratik.shetty@investree.id>
2020-11-09 11:00:14 +0530

    delete all contents

--- src/main/java/ind/automation/salesexpense/service/BankStatementConversionService.java
+++
@@ -1,9 +0,0 @@
-package ind.automation.salesexpense.service;
-
-import java.io.IOException;
-import org.springframework.web.multipart.MultipartFile;
-import ind.automation.salesexpense.responsebeans.BankStatementResponseListResponse;
-
-public interface BankStatementConversionService {
-	BankStatementResponseListResponse convertBankStatement(MultipartFile file) throws IOException;
-}
